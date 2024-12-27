1bda34fb12a70e1ba6030e40b3fe797d61f82ee1
Aaron Huang <aaronhuang0000@gmail.com>
2024-12-27 13:32:56 -0500

    additional bg image - background image for top * - used top_page.css for the background image

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def get_token(): #to refresh token and check if theres even a token
 
 def create_spotifyOuth() : #everytime use object use a new one
     return SpotifyOAuth(client_id ="e58f127ad4a0477bb887ebd3fbec37e3",
-                                                client_secret="76fdc0cb18e84cd2b69905d9544bd431",
+                                                client_secret="b3ff62e4577644e293082c8511ca50e2",
                                                 redirect_uri=url_for("redirectPage", _external=True), #url_for is a good way to not hardcode the path _external=True will create an absolute path
                                                 scope=scopes_used)
 '''
