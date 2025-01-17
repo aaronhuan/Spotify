f6c0eb3f820e891d0a85900e9274864e02f7b99a
Aaron Huang <aaronhuang0000@gmail.com>
2025-01-16 23:40:25 -0500

    added image info + start on modifying top tracks - make tracks into similar format as artists

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def get_token(): #to refresh token and check if theres even a token
 
 def create_spotifyOuth() : #everytime use object use a new one
     return SpotifyOAuth(client_id ="e58f127ad4a0477bb887ebd3fbec37e3",
-                                                client_secret="f419cf6e443e4643b3f80e08070b9848",
+                                                client_secret="062c487907604e21a987a29fcf2d3c08",
                                                 redirect_uri=url_for("redirectPage", _external=True), #url_for is a good way to not hardcode the path _external=True will create an absolute path
                                                 scope=scopes_used)
 '''
